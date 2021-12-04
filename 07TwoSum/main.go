fb0dea7bdafe1dfec07301abba353174b31c724d
Steven-hsm <1586558083@qq.com>
2021-12-04 16:11:14 +0800

    算法题

--- 07TwoSum/main.go
+++ 07TwoSum/main.go
@@ -13,3 +13,20 @@ func twoSum(nums []int, target int) []int {
 	}
 	return []int{}
 }
+
+func twoSum2(nums []int, target int) []int {
+	numMap := make(map[int]int, len(nums))
+	for index, num := range nums {
+		sencondNum := target - num
+		if _, ok := numMap[sencondNum]; ok {
+			if index == numMap[sencondNum] {
+				continue
+			} else if index > numMap[sencondNum] {
+				return []int{numMap[sencondNum], index}
+			}
+			return []int{index, numMap[sencondNum]}
+		}
+		numMap[num] = index
+	}
+	return []int{0, 0}
+}
